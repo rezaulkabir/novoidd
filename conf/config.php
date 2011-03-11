a90941957276c5980b5c007d9fdcf4692a2a3c9a
Rezaul Kabir <rezaulkabir@gmail.com>
2011-03-11 19:56:21 +0600

    First upload

---
+++ conf/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+// Include DAL
+require_once(dirname(dirname(__FILE__)) . '/inc/class/search.php');
+
+// Database
+define ( 'DB_HOST', 'localhost' );
+define ( 'DB_USER', 'root' );
+define ( 'DB_PASSWORD', '' );
+define ( 'DB_DB', 'ratedb' );
+
+?>
